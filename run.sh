7a0496d81b806a67c4d2605e16204f5ed75923d4
bitmagier <bitmagier@mailbox.org>
2020-11-08 21:33:59 +0100

    cleanup

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-export SBT_OPTS="-Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC" # -XX:+UseG1GC"
+export SBT_OPTS="-Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC"
 sbt run 2>&1 | tee /tmp/arbitrage-trader-run$(date "+%Y-%m-%dT%H:%M"|tr -d '\n').log
 
